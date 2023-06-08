8f3cad00b4139defc495f958ca759db2b7493b44
PRADEESHAVAIRAM <pradeesha.vairam@kprinfo.com>
2023-06-08 18:07:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 4 },
   ])
 
   return (
